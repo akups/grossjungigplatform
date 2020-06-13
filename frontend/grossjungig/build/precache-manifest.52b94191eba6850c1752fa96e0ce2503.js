38e7eb455465ba99b1a315730571b3f72e71fc44
akups <appiah-lindberg@outlook.com>
2020-06-13 19:03:33 +0200

    build added

---
+++ frontend/grossjungig/build/precache-manifest.52b94191eba6850c1752fa96e0ce2503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b3229950ea530a39aa5a344bfe2e87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce108b0fb8afec05f4c",
+    "url": "/static/css/main.26db92d7.chunk.css"
+  },
+  {
+    "revision": "58a3d637419a6db6a5c6",
+    "url": "/static/js/2.dde156c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dde156c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce108b0fb8afec05f4c",
+    "url": "/static/js/main.22991a0e.chunk.js"
+  },
+  {
+    "revision": "d4f7d70172481223be70",
+    "url": "/static/js/runtime-main.55011264.js"
+  }
+]);
