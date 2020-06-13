9bef5d744b11be742e00edf6cef26f65fdd63628
akups <appiah-lindberg@outlook.com>
2020-06-13 18:38:36 +0200

    mongo url

--- db/index.js
+++ db/index.js
@@ -5,17 +5,21 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
+//mongodb://heroku_r5f3x44q:6f05c4sqstqspd01n62kv7bish@ds159509.mlab.com:59509/heroku_r5f3x44q
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(conn => {
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb:////heroku_r5f3x44q:6f05c4sqstqspd01n62kv7bish@ds159509.mlab.com:59509/heroku_r5f3x44q"
+  )
+  .then((conn) => {
     console.log(`Connected to ${conn.connections[0].name}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`Error connecting to the DB: ${err}`);
   });
 
 module.exports = {
   disconnect: () => {
     mongoose.connection.close();
-  }
+  },
 };
